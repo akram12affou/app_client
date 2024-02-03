8ce80979d3216998e944d868182167dc84f8db9a
Akram Affou <97305449+akram12affou@users.noreply.github.com>
2024-02-03 12:25:37 +0100

    'change'

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
+import { Fragment } from "react";
 
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
 
-
+import Mainlayout from './components/Mainlayout'
+import AboutPage from "./pages/About/AboutPage";
+import HomePage from './pages/Home/HomePage'
 
 function App() {
 
-
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Mainlayout />,
+        errorElement:'error',
+        children: [
+          {
+            path: "about",
+            element: <AboutPage />,
+          },
+          {
+            index:true,
+            element:<HomePage/>
+          },
+          // {
+          //   path: "contact",
+          //   element:<ContactPage/>
+          // }
+        ],
+      },
+    ]
+  );
+  
   return (
-    <>
-       hru
-    </>
+    <Fragment>
+        <RouterProvider router={router} />
+    </Fragment>
   )
 }
 
